707548bf4551a8f792cbda0c77fd07b633b05577
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2024-04-28 16:54:32 +0200

    * improved tts

--- lfffxivtts/tts.py
+++ lfffxivtts/tts.py
@@ -1,41 +1,94 @@
+import io
 import json
-import subprocess
-import tempfile
-import platform
-import sounddevice as sd
-import soundfile as sf
+import math
 import os
-from os.path import join
-from voice import select_voice
 import queue
+import tempfile
 import threading
 import time
+from os.path import join
+
+import sounddevice as sd
+import soundfile as sf
+from pydub import AudioSegment
+
+from voice import select_voice
+
+
+def run_piper(config, payload, model, language, speaker, temp_dir, file_name_template):
+    model_file = os.path.abspath("piper/models/" + model + "_" + language + ".onnx")
+    config_file = os.path.abspath("piper/models/" + model + "_" + language + ".json")
+    voice_config = {}
+    model_lang = model + "_" + language
+
+    if model_lang in config["tts"]["modelConfig"]:
+        voice_config = config["tts"]["modelConfig"][model_lang]
+    with open(config_file, "r") as f:
+        model_config = json.load(f)
+
+    from piper import PiperVoice
+
+    voice = PiperVoice.load(model_file, config_path=config_file, use_cuda=False)
+
+    phonemes = voice.phonemize(payload)
+    output_files = []
+    # with wave.open(wav_file, "wb") as wav_file:
+
+    # wav_file.setframerate(model_config["audio"]["sample_rate"])
+    # wav_file.setsampwidth(2)  # 16-bit
+    # wav_file.setnchannels(1)  # mono
+
+    for index, sentence_ in enumerate(phonemes):
+        min_phoneme_count = voice_config["minPhonemeCount"] if "minPhonemeCount" in voice_config else 0
+        noise_scale = None
+
+        if len(sentence_) >= min_phoneme_count:
+            sentence = sentence_
+            n_repetitions = 1
+        else:
+            n_repetitions = int(math.ceil(1.0 * min_phoneme_count / len(sentence_)))
+            print("[i] Min phoneme count", min_phoneme_count, "not reached. Repeating", "".join(sentence_), "x",
+                  n_repetitions)
+            sentence = (sentence_ + [";", ",", " "]) * n_repetitions
+            if "phonemeCountFixNoiseScale" in voice_config:
+                noise_scale = voice_config["phonemeCountFixNoiseScale"]
+
+        synthesize_args = {
+            "phoneme_input": True,
+            "speaker_id": int(speaker),
+            "length_scale": None,
+            "noise_scale": noise_scale,
+            "noise_w": None,
+            "sentence_silence": 0.2,
+        }
+        print("[i] TTS >", "".join(sentence))
+
+        wav_bytes = b"".join(list(voice.synthesize_stream_raw("".join(sentence), **synthesize_args)))
+        if n_repetitions > 1:
+            width = int(len(wav_bytes) / n_repetitions)
+            wav_bytes = wav_bytes[:width]
+
+        bytes_io = io.BytesIO(wav_bytes)
+        segment = AudioSegment.from_raw(bytes_io, frame_rate=model_config["audio"]["sample_rate"], sample_width=2,
+                                        channels=1)
+
+        wav_file = join(temp_dir, file_name_template + str(index) + ".wav")
+        try:
+            new_segment = segment.fade_out(100) + AudioSegment.silent(duration=150)
+            new_segment.export(wav_file, format="wav")
+        except:
+            new_segment = segment + AudioSegment.silent(duration=150)
+            new_segment.export(wav_file, format="wav")
+
+        output_files.append(wav_file)
+
+    return output_files
 
 
 def get_wav_duration(file_path):
     with sf.SoundFile(file_path, 'r') as f:
         duration = len(f) / f.samplerate
     return duration
-
-
-def run_piper(payload, model, language, speaker, wav_file):
-    model_file = os.path.abspath("piper/models/" + model + "_" + language + ".onnx")
-    config_file = os.path.abspath("piper/models/" + model + "_" + language + ".json")
-
-    if platform.system() == 'Linux':
-        executable = "piper/piper_linux_x86_64/piper"
-    else:
-        print("[!] Unknown operating system")
-        return
-
-    executable = os.path.abspath(executable)
-
-    command = [executable, "--model", model_file, "--config", config_file, "--output_file", wav_file, "--speaker",
-               speaker]
-    print(payload, ">", " ".join(command))
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, cwd=os.path.dirname(executable))
-    process.communicate(payload.encode("utf-8"))
-    process.wait()
 
 
 def tts(config, payload, npc_id, full_name):
@@ -48,7 +101,8 @@ def tts(config, payload, npc_id, full_name):
     with tempfile.TemporaryDirectory() as temp_dir:
         piper_wav_file = join(temp_dir, "piper.wav")
 
-        run_piper(payload, voice["model"], voice["language"], voice["speaker"], piper_wav_file)
+        piper_wav_files = run_piper(config, payload, voice["model"], voice["language"],
+                                    voice["speaker"], temp_dir, "piper")
 
         # Voicefixer
         if "voicefixer" in config:
@@ -58,8 +112,9 @@ def tts(config, payload, npc_id, full_name):
                 tts_voicefixer_whole(config, piper_wav_file, temp_dir)
         else:
             # Playback of the original inference
-            wav_data, sample_rate = sf.read(piper_wav_file)
-            sd.play(wav_data * config["volume"], samplerate=sample_rate, blocking=True)
+            for wav_file in piper_wav_files:
+                wav_data, sample_rate = sf.read(wav_file)
+                sd.play(wav_data * config["volume"], samplerate=sample_rate, blocking=True)
 
 
 def tts_voicefixer_splitting(config, piper_wav_file, temp_dir):
@@ -115,6 +170,7 @@ def tts_voicefixer_splitting(config, piper_wav_file, temp_dir):
     # Wait for the processing thread to finish
     processing_thread.join()
     audio_thread.join()
+
 
 def tts_voicefixer_whole(config, piper_wav_file, temp_dir):
     print("[i] Applying voicefixer (whole audio) ...")
